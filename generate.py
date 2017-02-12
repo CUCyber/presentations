a55c4114b20b4b954802cceaa0a64a70e59703c4
Foster McLane <fkmclane@gmail.com>
2017-02-12 18:12:07 -0500

    make generate file more pythonic

--- generate.py
+++ generate.py
@@ -1,43 +1,50 @@
 #!/usr/bin/env python3
-import argparse
 import os
 import os.path
 import shutil
 import urllib.parse
 
-parser = argparse.ArgumentParser(description='generate HTML directory from reveal.js Markdown source')
-parser.add_argument('-i', '--id', dest='id', default='presentation', help='reveal.js id in website')
-parser.add_argument('-r', '--root', dest='root', default='/', help='reveal.js root in website')
-parser.add_argument('-o', '--theme', dest='theme', default='simple', help='reveal.js theme to use')
-parser.add_argument('-t', '--template', dest='template', default='template.html', help='template file to generate from')
-parser.add_argument('-a', '--res', dest='res', default='', help='resource folder for presentation')
-parser.add_argument('infile', nargs='?', default='presentation.md', help='input markdown file')
-parser.add_argument('outdir', nargs='?', default='.', help='output directory')
 
-args = parser.parse_args()
+def generate(input_file, reveal_id, resource_dir, template, root_dir, output_dir):
+    # copy resources or make an empty directory
+    if os.path.exists(resource_dir):
+        shutil.copytree(resource_dir, output_dir)
+    else:
+        os.makedirs(output_dir)
 
-# copy resources or make an empty directory
-if os.path.exists(args.res):
-    shutil.copytree(args.res, args.outdir)
-else:
-    os.makedirs(args.outdir)
+    # copy file to output directory
+    shutil.copy(input_file, output_dir)
 
-# copy file to output directory
-shutil.copy(args.infile, args.outdir)
+    with open(os.path.join(output_dir, 'index.html'), 'w') as index:
+        with open(template, 'r') as template:
+            # get the title
+            with open(input_file, 'r') as presentation:
+                title = presentation.readline()
 
-with open(os.path.join(args.outdir, 'index.html'), 'w') as index:
-    with open(args.template, 'r') as template:
-        # get the title
-        with open(args.infile, 'r') as presentation:
-            title = presentation.readline()
+            # clean up the title
+            title = title.strip()
 
-        # clean up the title
-        title = title.strip()
+            if title[0] == '#':
+                title = title[1:]
 
-        if title[0] == '#':
-            title = title[1:]
+            title = title.strip()
 
-        title = title.strip()
+            # template given file
+            index.write(template.read().format(id=urllib.parse.quote(reveal_id, safe=''), title=title, root=urllib.parse.quote(root_dir), theme=args.theme, presentation=os.path.basename(input_file)))
 
-        # template given file
-        index.write(template.read().format(id=urllib.parse.quote(args.id, safe=''), title=title, root=urllib.parse.quote(args.root), theme=args.theme, presentation=os.path.basename(args.infile)))
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='generate HTML directory from reveal.js Markdown source')
+    parser.add_argument('-i', '--id', dest='id', default='presentation', help='reveal.js id in website')
+    parser.add_argument('-r', '--root', dest='root', default='/', help='reveal.js root in website')
+    parser.add_argument('-o', '--theme', dest='theme', default='simple', help='reveal.js theme to use')
+    parser.add_argument('-t', '--template', dest='template', default='template.html', help='template file to generate from')
+    parser.add_argument('-a', '--res', dest='res', default='', help='resource directory for presentation')
+    parser.add_argument('infile', nargs='?', default='presentation.md', help='input markdown file')
+    parser.add_argument('outdir', nargs='?', default='.', help='output directory')
+
+    args = parser.parse_args()
+
+    generate(args.infile, args.id, args.res, args.template, args.root, args.outdir)
